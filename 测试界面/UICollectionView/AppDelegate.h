227b567f4f1b28462178f11e55eadc9554b131c4
贺嘉炜 <hejiawei@lvmama.com>
2017-03-27 17:41:21 +0800

    first commit

---
+++ 测试界面/UICollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICollectionView
+//
+//  Created by 贺嘉炜 on 2017/3/27.
+//  Copyright © 2017年 贺嘉炜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 测试界面/UICollectionView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UICollectionView
+//
+//  Created by 贺嘉炜 on 2017/3/27.
+//  Copyright © 2017年 贺嘉炜. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
