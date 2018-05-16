c94661d563210c80a12ea85772238e7445391873
hejiawei <hejiawei@lvmama.com>
2018-05-16 16:25:35 +0800

    报错修改

--- 测试界面/测试界面/AppDelegate.h
+++ 测试界面/测试界面/AppDelegate.h
@@ -2,8 +2,8 @@
 //  AppDelegate.h
 //  测试界面
 //
-//  Created by 贺嘉炜 on 2017/4/27.
-//  Copyright © 2017年 贺嘉炜. All rights reserved.
+//  Created by 贺嘉炜 on 2018/5/16.
+//  Copyright © 2018年 贺嘉炜. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
