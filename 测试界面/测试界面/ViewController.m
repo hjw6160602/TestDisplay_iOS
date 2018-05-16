38c33e7ed32c43a1738e21c11593c8a2fc6e8c08
hejiawei <hejiawei@lvmama.com>
2018-05-16 16:27:14 +0800

    reuseIdentifier为空情况下创建cell测试

--- 测试界面/测试界面/ViewController.m
+++ 测试界面/测试界面/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
 }
 
 
