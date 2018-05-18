d76246208f3161db7e31d7cc4bb450043f35536c
hejiawei <hejiawei@lvmama.com>
2018-05-18 10:22:05 +0800

    添加Cell

---
+++ 测试界面/地图测试Demo/DisCoverAroundTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  DisCoverAroundTableViewCell.m
+//  地图测试Demo
+//
+//  Created by 贺嘉炜 on 2018/5/17.
+//  Copyright © 2018年 贺嘉炜. All rights reserved.
+//
+
+#import "DisCoverAroundTableViewCell.h"
+
+@implementation DisCoverAroundTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
